70974f64bbb424e693587da812258d96055e1256
shubham-afk <sahilraj2310@gmail.com>
2025-12-15 12:35:15 +0530

    db: create video model

---
+++ src/models/video.models.js
@@ -0,0 +1,53 @@
+import mongoose, { Schema } from "mongoose";
+
+/**
+ * owner ObjectId users
+  videoFile string
+  thumbnail string
+  title string
+  description string
+  duration number
+  views number
+  isPublished boolean
+  createdAt Date
+  updatedAt Date
+ */
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
