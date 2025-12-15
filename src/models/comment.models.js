06b015b9c1bc2f05d5bf1f969c5cdea13ed8f300
shubham-afk <sahilraj2310@gmail.com>
2025-12-15 13:49:39 +0530

    model: create comment model

---
+++ src/models/comment.models.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
