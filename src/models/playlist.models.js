3e5a01fe04a564b90712a8002b44c8f502e5120d
shubham-afk <sahilraj2310@gmail.com>
2025-12-15 13:50:12 +0530

    model: create playlist model

---
+++ src/models/playlist.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
