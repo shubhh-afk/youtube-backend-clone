cb444637f21b1efe7e7d36639ed6b35b946538fd
shubhh-afk <sahilraj2310@gmail.com>
2026-01-14 13:17:07 +0530

    feat: user login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,28 @@ import {
   deleteFromCloudinary,
 } from "../utils/cloudinary.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating Refresh token and Access token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // TODO
   const { fullname, email, username, password } = req.body;
@@ -95,6 +117,51 @@ const registerUser = asyncHandler(async (req, res) => {
       "Something went wrong while registering a user and images were deleted"
     );
   }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!email) {
+    throw new ApiError(400, "Email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(400, "User not found");
+  }
+
+  const isPasswordValid = user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(400, "Password is not valid");
+  }
+
+  const { refreshToken, accessToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!loggedInUser) {
+    throw new ApiError(400, "Something went wrong while logging in the user");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
 });
 
 export { registerUser };
